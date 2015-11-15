1bbdce18e2645fff62567db0f4b13d3440ee36cc
jmwaller <jwall@TheGrey.eau.wi.charter.com>
2015-11-15 13:42:20 -0600

    Added test for RoundPlus()
    Added test for RoundPlus()

--- mymath_test.go
+++ mymath_test.go
@@ -16,12 +16,31 @@ func TestRound(t *testing.T) {
 	for _, c := range cases {
 		got := Round(c.in)
 		if got != c.want {
-			t.Errorf("Reverse(%f) == %f, want %f", c.in, got, c.want)
+			t.Errorf("Round(%f) == %f, want %f", c.in, got, c.want)
 		}
 	}
 }
 
 // RoundPlus -- Returns value rounded to nearest places significant digits.
-//func TestRoundPlus(t *testing.T) {
+func TestRoundPlus(t *testing.T) {
+	cases := []struct {
+		in1  float64
+		in2  int
+		want float64
+	}{
+		{100.5, 0, 101},
+		{100.499999999, 2, 100.50},
+		{-0.77777, 4, -0.7778},
+		{-0.11111, 0, 0},
+		{0, 0, 0},
+	}
+	for _, c := range cases {
+		got := RoundPlus(c.in1, c.in2)
+		if got != c.want {
+			t.Errorf("RoundPlus(%f, %d) == %f, want %f", c.in1, c.in2, got, c.want)
+		}
+	}
+}
+
 // RoundInPlace -- Replaces value with rounded whole number value.
 //func TestRoundInPlace(t *testing.T) {
